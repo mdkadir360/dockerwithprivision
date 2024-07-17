20aa0f8531e4389a3297f6bcbbf04496eaf8a3e7
Abdul Kadir <mdkadir360@gmail.com>
2024-07-17 10:48:58 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,98 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources1"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                      = "example-network-interface1"
+  location                  = azurerm_resource_group.example.location
+  resource_group_name       = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  admin_password = "adminuser@1234"
+  disable_password_authentication = false
+
+
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+ provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y docker.io",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+
+      "git clone https://github.com/devopsinsiders/ReactTodoUIMonolith.git /tmp/ReactTodoapp",
+       "git clone https://github.com/devopsinsiders/PyTodoBackendMonolith.git /tmp/PyTodoapp"
+    ]
+
+connection {
+  host = self.public_ip_address
+  user = self.admin_username
+  password = self.admin_password
+  type = "ssh"
+  timeout = "4m"
+}
+
+
+
+
+}
+
+provisioner "local-exec" {
+  command = "echo Everything > done.txt"
+  
+}
+
+
+}
